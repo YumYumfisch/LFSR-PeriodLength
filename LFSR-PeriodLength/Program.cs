00473ad552f492e8a0b85e3e0aa44c95d654dde7
YumYumFisch <77779732+YumYumfisch@users.noreply.github.com>
2024-10-11 19:09:57 +0200

    work in progress

--- LFSR-PeriodLength/Program.cs
+++ LFSR-PeriodLength/Program.cs
@@ -1,11 +1,16 @@
 ﻿namespace LFSR_PeriodLength;
 
-internal class Program
+public class Program
 {
     /// <summary>
     /// Size of the Matrix in the CSV file
     /// </summary>
     static int MatrixSize { get; } = 128;
+
+    /// <summary>
+    /// Number of LfsrRegisters for fibonacci and galois LFSR
+    /// </summary>
+    private static int LfsrRegisterCount { get; } = 4;
 
     /// <summary>
     /// Application entry point
@@ -15,30 +20,65 @@ internal class Program
     {
         _ = args; // unused
 
-        string filePath = "lfsr_periods.csv";
+        uint state = 1;
+        uint tap = 0b1001;
+        Console.WriteLine(state);
+        for (int i = 0; i < LfsrRegisterCount * LfsrRegisterCount; i++)
+        {
+            state = FibonacciNextState(state, tap);
+            Console.WriteLine(state);
+        }
+        Console.WriteLine("Period: " + LfsrPeriod(1, tap, FibonacciNextState));
 
-        WritePeriodMatrixToCsv(filePath);
+        WritePeriodMatrixToCsv("modulo_periods.csv", ModuloNextState);
+        WritePeriodMatrixToCsv("fibonacci_periods.csv", FibonacciNextState);
+        //WritePeriodMatrixToCsv("galois_periods.csv", GaloisNextState);
     }
 
     /// <summary>
     /// Calculates the next state of the LSFR
     /// </summary>
-    /// <param name="state">Current State</param>
-    /// <param name="modulo">Modulo value</param>
+    /// <param name="state">Current state of the LFSR registers</param>
+    /// <param name="functionModifier">Modifier that modifies the function of this LSFR</param>
     /// <returns>Next state of the LSFR</returns>
-    static uint NextState(uint state, uint modulo)
+    private delegate uint NextState(uint state, uint functionModifier);
+
+    /// <summary>
+    /// Calculates the next state of the modulo LSFR
+    /// </summary>
+    /// <param name="state">Current state of the LFSR registers</param>
+    /// <param name="modulo">Modulo value</param>
+    /// <returns>Next state of the modulo LSFR</returns>
+    private static uint ModuloNextState(uint state, uint modulo)
     {
         state <<= 1;
         return state % modulo;
     }
 
     /// <summary>
+    /// Calculates the next state of the fibonacci LSFR
+    /// </summary>
+    /// <param name="state">Current state of the LFSR registers</param>
+    /// <param name="tapMask">Masks which registers are used for the linear function of the LSFR</param>
+    /// <returns>Next state of the fibonacci LSFR</returns>
+    private static uint FibonacciNextState(uint state, uint tapMask)
+    {
+        uint leftmostBit = state ^ tapMask;
+        leftmostBit = (leftmostBit % 2) << (LfsrRegisterCount - 1);
+
+        state >>= 1;
+
+        return state | leftmostBit;
+    }
+
+    /// <summary>
     /// Calculates the period length of the LSFR
     /// </summary>
-    /// <param name="modulo">Modulo value of the linear function</param>
-    /// <param name="startValue">Start value of the shift register</param>
+    /// <param name="startValue">Start configuration of the LFSR</param>
+    /// <param name="functionModifier">Modifier that modifies the function of this LSFR</param>
+    /// <param name="function">Linear function of this LSFR</param>
     /// <returns>The period length of the LSFR with the specified values</returns>
-    static int LfsrPeriod(uint modulo, uint startValue)
+    private static int LfsrPeriod(uint startValue, uint functionModifier, NextState function)
     {
         uint state = startValue;
         List<uint> seenStates = [];
@@ -56,7 +96,7 @@ internal class Program
             }
 
             seenStates.Add(state);
-            state = NextState(state, modulo);
+            state = function(state, functionModifier);
         }
 
         if (state != seenStates[0])
@@ -78,24 +118,25 @@ internal class Program
     /// Creates the LSFR Period Matrix CSV File
     /// </summary>
     /// <param name="filePath">Path of the CSV file</param>
-    static void WritePeriodMatrixToCsv(string filePath)
+    /// <param name="function">Linear function of this LSFR</param>
+    private static void WritePeriodMatrixToCsv(string filePath, NextState function)
     {
         List<string> lines = [];
 
-        List<uint> oneToEnd = [];
+        List<uint> modifiers = [];
         for (uint i = 1; i <= MatrixSize; i++)
         {
-            oneToEnd.Add(i);
+            modifiers.Add(i);
         }
-        lines.Add(" ," + string.Join(",", oneToEnd));
+        lines.Add(" ," + string.Join(",", modifiers));
 
         for (uint startValue = 1; startValue <= MatrixSize; startValue++)
         {
             List<string> line = [];
 
-            for (uint modulo = 1; modulo <= MatrixSize; modulo++)
+            for (uint functionModifier = 1; functionModifier <= MatrixSize; functionModifier++)
             {
-                int periodLength = LfsrPeriod(modulo, startValue);
+                int periodLength = LfsrPeriod(startValue, functionModifier, function);
                 if (periodLength < 0)
                 {
                     line.Add(" ");
