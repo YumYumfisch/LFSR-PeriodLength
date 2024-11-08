3357b7da41f4651a19fa7ca85009a62eee91a144
YumYumFisch <77779732+YumYumfisch@users.noreply.github.com>
2024-11-08 16:55:10 +0100

    export period registers and register periods to csv files

--- CSV-PeriodLength/Program.cs
+++ CSV-PeriodLength/Program.cs
@@ -47,6 +47,13 @@ public static class Program
             Log.Information("{registerPadding}{registers} registers: {periods}", new string(' ', 2 - registerCount.ToString().Length), registerCount, registerPeriods[registerCount - 1]);
         }
 
+        List<string> registerPeriodsLines = [];
+        for (int i = 0; i < registerPeriods.Length; i++)
+        {
+            registerPeriodsLines.Add($"{i},{string.Join(',', registerPeriods[i])}");
+        }
+        File.WriteAllLines("registerPeriods.csv", registerPeriodsLines);
+
         Log.Information("Possible register count to get each period, ordered by minimum number of registers:");
 
         Dictionary<int, List<int>> periodRegisters = [];
@@ -69,17 +76,23 @@ public static class Program
             }
         }
 
+        List<string> periodRegistersByRegisterLines = [];
         foreach (KeyValuePair<int, List<int>> period in periodRegisters)
         {
             Log.Information("Period {periodPadding}{period}: {registers}", new string(' ', 5 - period.Key.ToString().Length), period.Key, period.Value);
+            periodRegistersByRegisterLines.Add($"{period.Key},{string.Join(',', period.Value)}");
         }
+        File.WriteAllLines("periodRegisters-byRegister.csv", periodRegistersByRegisterLines);
 
         Log.Information("Possible register count to get each period, ordered by period:");
 
+        List<string> periodRegistersByPeriodLines = [];
         IOrderedEnumerable<KeyValuePair<int, List<int>>> orderedPeriodRegisters = periodRegisters.OrderBy(periodRegister => periodRegister.Key);
         foreach (KeyValuePair<int, List<int>> period in orderedPeriodRegisters)
         {
             Log.Information("Period {periodPadding}{period}: {registers}", new string(' ', 5 - period.Key.ToString().Length), period.Key, period.Value);
+            periodRegistersByPeriodLines.Add($"{period.Key},{string.Join(',', period.Value)}");
         }
+        File.WriteAllLines("periodRegisters-byPeriod.csv", periodRegistersByPeriodLines);
     }
 }
